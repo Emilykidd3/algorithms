80dc9c330a9b12404e7f632d3fd6e1231b4fcfca
emilykidd3 <e.kidd61@yahoo.com>
2021-03-29 23:47:58 -0700

    added psuedocode for algorithm 3

---
+++ 03-multiply-into-20/unsolved.js
@@ -0,0 +1,8 @@
+// make a for loop that loops through the given array
+// make another for loop withing that loop that compares the number from the first for loop to each of the other(or all) numbers in the array
+// if the first loop number times the second loop number equals 20, return true
+// if it doesnt, continue the second loop 
+// if none of the numbers multiply to twenty the first for loop will go to the next number
+// loop through all the numbers in the first loop
+// if any multiply to equal 20, return true
+// in none multiply to equal 20, return false
