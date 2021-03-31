8719b04d96d95e86565d211a185ee339af703423
emilykidd3 <e.kidd61@yahoo.com>
2021-03-30 17:21:31 -0700

    added binary search to alg 4

--- 04-find-index/unsolved.js
+++ 04-find-index/unsolved.js
@@ -12,4 +12,26 @@ const findIndex = function(givenNumber) {
     }
 }
 
-findIndex(99);
+// findIndex(99);
+
+//binary search
+
+const binarySearch = (arr, num, left, right) => {
+    let middle = Math.floor((left + right) / 2);
+
+    // range overlapped, so never found number
+    if (left > right) {
+        return -1;
+    } else if (num === arr[middle]) {
+        return middle;
+    } else if (num < arr[middle]) {
+        // call again with a new right value
+        return binarySearch(arr, num, left, middle - 1);
+    } else {
+        // call again with a new left value
+        return binarySearch(arr, num, middle + 1, right);
+    }
+};
+
+// set initial left and right values on first call
+console.log(binarySearch(data, 40, 0, data.length - 1));
