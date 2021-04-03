32e514e54731d203d00e83875082f6c5c9a0f261
emilykidd3 <e.kidd61@yahoo.com>
2021-04-02 21:27:51 -0700

    solved algorithm 2

--- 02-is-palindrome/unsolved.js
+++ 02-is-palindrome/unsolved.js
@@ -7,3 +7,21 @@
 // compare arrays
 // if they are equal return true
 // if they are not equal return false
+
+
+
+const isPalindrome = function(strng) {
+    var newArr = [];
+    var arr = strng.split("");
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i]);
+    }
+    if (arr.join('') === newArr.join('')) {
+        console.log("true");
+        return true;
+    } 
+    console.log("false")
+    return false;
+}
+
+isPalindrome("racecar");
