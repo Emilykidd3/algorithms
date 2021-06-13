adbbe39c4c812268236163cef206086a6e660926
emilykidd3 <e.kidd61@yahoo.com>
2021-06-12 21:57:54 -0700

    alg will return 0 if array is empty

--- 09-/Unsolved/missing-number.js
+++
@@ -1,6 +0,0 @@
-// sort array
-// loop through array to find missing number
-// if there is no missing number, use the next number in the array
-// if the array is empty return 0
-
-var missingNumber = function(nums) {};
---
+++ 09-missing-number/Unsolved/missing-number.js
@@ -0,0 +1,19 @@
+// sort array
+// loop through array to find missing number
+// if there is no missing number, use the next number in the array
+// if the array is empty return 0
+
+var arr = []
+
+var missingNumber = function(nums) {
+    if (nums = []) {
+        console.log(0);
+        return 0;
+    }
+
+    // sort array
+
+    // loop array
+};
+
+missingNumber(arr);
