8a9275f21a59fa0ac33e338e97868a4cfa3a3447
emilykidd3 <e.kidd61@yahoo.com>
2021-06-12 21:48:21 -0700

    added psuedocode

--- 09-/Unsolved/missing-number.js
+++ 09-/Unsolved/missing-number.js
@@ -1,3 +1,6 @@
-// Write a function that takes an unsorted array of positive and unique integers and returns the number missing from the array
+// sort array
+// loop through array to find missing number
+// if there is no missing number, use the next number in the array
+// if the array is empty return 0
 
 var missingNumber = function(nums) {};
