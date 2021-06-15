3702d4715a081a0dfcdb967d7b1629bf72797ccb
emilykidd3 <e.kidd61@yahoo.com>
2021-06-14 20:55:45 -0700

    added if case for factorial alg

--- 10-factorial/Unsolved/factorial.js
+++ 10-factorial/Unsolved/factorial.js
@@ -1,3 +1,12 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var num = 0
+
+var factorial = function(num) {
+    if (num === 0) {
+        console.log(1);
+        return 1;
+    }
+};
+
+factorial(num)
