24ff38b23839e496849c6ca4dce321ef84336585
emilykidd3 <e.kidd61@yahoo.com>
2021-06-17 21:37:23 -0700

    pseudocoded left rotation alg

--- 11-left-rotation/Unsolved/left-rotation.js
+++ 11-left-rotation/Unsolved/left-rotation.js
@@ -2,4 +2,16 @@
 // Modify the original array rather than returning a new one
 // There is no need to return from this function
 
-var leftRotation = function(arr, positions) {};
+// pseudeocode
+// set a temporary variable for the first digit in the array
+// pop? the first digit off of the array
+// push the digit to the end of the array
+// loop as many times as the positions variable
+
+var arr = [0, 1, 2, 3]
+
+var positions = 3
+
+var leftRotation = function(arr, positions) {
+
+};
