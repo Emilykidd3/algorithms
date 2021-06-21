cec4ef21b93944ade0250aa8009dfb0fe2b4a5c3
emilykidd3 <e.kidd61@yahoo.com>
2021-06-20 21:30:28 -0700

    solved alg

--- 12-max-num/Unsolved/max-num.js
+++ 12-max-num/Unsolved/max-num.js
@@ -3,4 +3,14 @@
 // sort array
 // return last item in array
 
-var maxNum = function(arr) {};
+arr = [3, 1, 17, 5, 6]
+
+var maxNum = function(arr) {
+    var newArr = arr.sort((a,b) => {
+        return a-b;
+    });
+    console.log(newArr[newArr.length-1])
+    return arr[newArr.length-1]
+};
+
+maxNum(arr);
