6088074c15ec3b471c0fde45ec382d2a6bf098f2
emilykidd3 <e.kidd61@yahoo.com>
2021-06-21 21:16:49 -0700

    solved zeroes and ones alg

--- 13-zeroes-and-ones/Unsolved/zeroes-and-ones.js
+++ 13-zeroes-and-ones/Unsolved/zeroes-and-ones.js
@@ -2,4 +2,33 @@
 // Return true if there are an equal number of 0s and 1s
 // Else return false
 
-var zeroesAndOnes = function(str) {};
+// convert the string into an array
+// count how many 0s there are in the arr and assign it to a variable
+// count how many 1s there are in the arr and assign it to a variable
+// if the variables are equal return true
+// if they are not return false
+
+var str = "0011011"
+
+var zeroesAndOnes = function(str) {
+    var arr = str.split("");
+    var zero = 0
+    var one = 0
+    for (var i=0; i < arr.length; i++) {
+        if (arr[i] === "0") {
+            zero++
+        }
+        else if (arr[i] === "1") {
+            one++
+        }
+    }
+    if (one === zero) {
+        console.log("true")
+        return true;
+    } else {
+        console.log("false")
+        return false;
+    }
+};
+
+zeroesAndOnes(str)
