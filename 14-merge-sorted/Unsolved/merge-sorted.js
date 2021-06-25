ef47493352d438896bb6c7227b1fd95f48eeb3e8
emilykidd3 <e.kidd61@yahoo.com>
2021-06-24 21:42:29 -0700

    pseudocoded alg 14

--- 14-merge-sorted/Unsolved/merge-sorted.js
+++ 14-merge-sorted/Unsolved/merge-sorted.js
@@ -1,3 +1,8 @@
 // Write code to merge two sorted arrays into a new sorted array
 
+// 
+
+var arr1 = [4, 8, 15, 16, 23, 42];
+var arr2 = [1, 2, 3, 6, 20, 21]; 
+
 var mergeSorted = function (arr1, arr2) {};
