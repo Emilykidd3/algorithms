918d9062756ab9049c1380d6c08fd0db0cc127c9
emilykidd3 <e.kidd61@yahoo.com>
2021-06-26 21:53:48 -0700

    started ransom-note alg

--- 15-ransom-note/Unsolved/ransom-note.js
+++ 15-ransom-note/Unsolved/ransom-note.js
@@ -1,9 +1,9 @@
 // Write a function that takes two strings and determines returns true if every word found in the second string is present in the first string. You will be checking for both words and their frequency. Assume you'll need to worry about casing, but the strings won't contain any punctuation. Assume neither string will be empty
 
 // split sentences to an array
-// loop through note arr
-// compare noteArr[1] to magArr if noteArr[1] !== magArr[i] return false
-// if not keep going
+// loop through noteArr
+// compare noteArr[1] to magArr if it matches any return 
+// if not return false
 // after full loop return true 
 
 var magazine = "hello world";
@@ -11,7 +11,18 @@ var note = "hello";
 // should return true
 
 var ransomNote = function(magazine, note) {
-
+    var magArr = magazine.split(" ");
+    var noteArr = note.split(" ");
+    for (var i = 0; i < noteArr.length; i++) {
+        for (var j = 0; j < magArr.length; j++) {
+            if (noteArr[i] === magArr[j]) {
+                return;
+            }
+            return;
+        }
+        console.log("true")
+        return true;
+    }
 };
 
 ransomNote(magazine, note)
