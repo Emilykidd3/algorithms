d3c305dea85759972b9aa9e46b2bd19848099a99
emilykidd3 <e.kidd61@yahoo.com>
2021-06-27 22:13:13 -0700

    pseudocoded new array

--- 16-squares-of-a-sorted-array/Unsolved/squares-of-a-sorted-array.js
+++ 16-squares-of-a-sorted-array/Unsolved/squares-of-a-sorted-array.js
@@ -1,3 +1,11 @@
 // Write code to create a function that accepts an array of integers sorted in ascending (increasing) order and returns a new array containing the squares of each number in ascending order
 
+// loop through array
+// multiply each item of array by itself and push to a new array
+// sort new array
+
+var arr = [-4, -1, 0, 3, 10];
+
+// should return [0, 1, 9, 16, 100];
+
 var sortedSquares = function(arr) {};
