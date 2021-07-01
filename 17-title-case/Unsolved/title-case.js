55640c986c896a717cd3b0d8fbf1dc63614d9a3c
emilykidd3 <e.kidd61@yahoo.com>
2021-06-30 21:39:00 -0700

    pseudocoded alg 17

--- 17-title-case/Unsolved/title-case.js
+++ 17-title-case/Unsolved/title-case.js
@@ -1,3 +1,8 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
+// split string into arr
+// loop through arr and capitalize each word 
+// join arr back into string with space
+// return
+
 var titleCase = function(str) {};
