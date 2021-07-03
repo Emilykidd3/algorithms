34079600235e6270d295d8cbf370b6c52355a147
emilykidd3 <e.kidd61@yahoo.com>
2021-07-03 11:54:00 -0700

    pseudocoded alg 18

--- 18-vowel-count/Unsolved/vowel-count.js
+++ 18-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,13 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+// make variable for vowelcount at 0
+// loop through string? 
+// if letter is === to a vowel vowelcount++
+// return count when loop is over
+
+var str = "emily"
+
+var vowelCount = function(str) {
+
+};
+
