efd73275c61c129394aab8a9b7cff515724d89e2
emilykidd3 <e.kidd61@yahoo.com>
2021-07-03 11:57:38 -0700

    solved alg 18

--- 18-vowel-count/Unsolved/vowel-count.js
+++ 18-vowel-count/Unsolved/vowel-count.js
@@ -8,6 +8,14 @@
 var str = "emily"
 
 var vowelCount = function(str) {
-
+    var vowelCount = 0
+    for (var i = 0; i < str.length; i ++) {
+        if (str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" || str[i] === "u" || str[i] === "y") {
+            vowelCount++
+        }
+    }
+    console.log(vowelCount);
+    return vowelCount;
 };
 
+vowelCount(str);
