6a427ce49340fe5e28bf4661f19c57a3d60c569f
emilykidd3 <e.kidd61@yahoo.com>
2021-07-04 20:50:22 -0700

    pseudocoded alg 19

--- 19-fizz-buzz/Unsolved/fizz-buzz.js
+++ 19-fizz-buzz/Unsolved/fizz-buzz.js
@@ -4,4 +4,12 @@
 // If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
 // If a number is not divisible by 3 or 5, print the number
 
+// loop through given arr
+// if number is divisible by 3 & 5 print fizz buzz
+// if number is divisible by 3 print fizz
+// if number is divisible by 5 print buzz
+// if else print number
+
+
+
 var fizzBuzz = function(arr) {};
