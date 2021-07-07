e45b0fc70ca62075d9406baab8fb8c231e071ba1
emilykidd3 <e.kidd61@yahoo.com>
2021-07-06 21:50:39 -0700

    pseudocoded alg 20

--- 20-swap-name/Unsolved/swap-case.js
+++ 20-swap-name/Unsolved/swap-case.js
@@ -1,3 +1,10 @@
 // Write code to create a function takes a string and returns the string with all of the letter cases swapped
 
+// loop through all letters
+// if letter is capitalized do tolowercase
+// if letter isnt do touppercase
+// push to new string
+// return string
+
+
 var swapCase = function(str) {};
