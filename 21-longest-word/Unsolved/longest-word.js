f6ccc719c30925b3d635ae75e0d84fd625725a3c
emilykidd3 <e.kidd61@yahoo.com>
2021-07-10 21:29:21 -0700

    pseudocoded alg

--- 21-longest-word/Unsolved/longest-word.js
+++ 21-longest-word/Unsolved/longest-word.js
@@ -1,3 +1,9 @@
 // Write code to create a function that accepts a string (`str`) and returns the longest word in the string
 
+// set var for longest word and var for length at 0?
+// loop throgh array 
+// loop through word and count how many letters
+// if amount of letters is creater than the var replace both vars
+// after full loop return longest word var
+
 var longestWord = function(str) {};
