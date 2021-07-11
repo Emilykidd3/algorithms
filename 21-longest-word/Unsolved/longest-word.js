abf2abf4478c4e5f02d5577fd1c0df14cb45f235
emilykidd3 <e.kidd61@yahoo.com>
2021-07-10 22:15:44 -0700

    solved alg 21

--- 21-longest-word/Unsolved/longest-word.js
+++ 21-longest-word/Unsolved/longest-word.js
@@ -6,4 +6,20 @@
 // if amount of letters is creater than the var replace both vars
 // after full loop return longest word var
 
-var longestWord = function(str) {};
+var str = "Which word is the longest word";
+
+var longestWord = function(str) {
+    var arr = str.split(" ")
+    var longestWord = ""
+    var longestWordLength = 0
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].length > longestWordLength) {
+            longestWord = arr[i]
+            longestWordLength = arr[i].length
+        }
+    }
+    console.log(longestWord);
+    return longestWord
+};
+
+longestWord(str)
