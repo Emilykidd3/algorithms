9273e161bfce63fead4aed7b180b053ff40f87c5
emilykidd3 <e.kidd61@yahoo.com>
2021-07-11 21:26:51 -0700

    pseudocoded alg 22

--- 22-acronym-builder/Unsolved/acronym-builder.js
+++ 22-acronym-builder/Unsolved/acronym-builder.js
@@ -1,3 +1,10 @@
 // Write code to create a function that accepts a string and returns an acronym for the given string
 
+// split str into an arr
+// loop through arr
+// take first letter of each word
+// add letter to new str
+// capitolize each letter
+// return new string
+
 var acronymBuilder = function(str) {};
