6ed1b638a9f28a1ab309a352eac56975b47ae20a
emilykidd3 <e.kidd61@yahoo.com>
2021-07-13 21:31:55 -0700

    pseudocoded alg 23

--- 23-reverse-word/Unsolved/reverse-words.js
+++ 23-reverse-word/Unsolved/reverse-words.js
@@ -1,3 +1,8 @@
 // Write code to create a function that accepts a string (`str`) and reverses the order of the words in the string
 
+// split str into arr
+// loop through arr backwards
+// push to new arr
+// join new arr and return
+
 var reverseWords = function(str) {};
