5bb0ce6d79f750106a8c27e55f46f880607eb0d4
emilykidd3 <e.kidd61@yahoo.com>
2021-07-17 20:35:31 -0700

    solved alg 24

--- 24-reverse-no-built-in/Unsolved/reverse-no-built-in.js
+++ 24-reverse-no-built-in/Unsolved/reverse-no-built-in.js
@@ -6,6 +6,15 @@
 // add each letter to the newStr
 // return newStr
 
-var reverse = function(str) {
+var str = "just keep swimming";
 
+var reverse = function(str) {
+    var newStr = "";
+    for (var i = str.length - 1; i > 0; i--) {
+        newStr += str[i];
+    }
+    console.log(newStr);
+    return newStr;
 };
+
+reverse(str);
