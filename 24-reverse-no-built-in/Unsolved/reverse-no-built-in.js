7fb5d7c5df0dd2aa20b91386b5349697647c34c7
emilykidd3 <e.kidd61@yahoo.com>
2021-07-17 20:27:55 -0700

    pseudocoded alg 24

--- 24-reverse-no-built-in/Unsolved/reverse-no-built-in.js
+++ 24-reverse-no-built-in/Unsolved/reverse-no-built-in.js
@@ -1,6 +1,11 @@
 // Write code to return a new string with all of the characters in `str` reversed.
 // You may NOT use the built-in reverse method
 
+// create newStr variable
+// loop through og str 
+// add each letter to the newStr
+// return newStr
+
 var reverse = function(str) {
 
 };
