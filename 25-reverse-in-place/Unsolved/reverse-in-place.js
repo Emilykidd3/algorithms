8ac9083e067539b3f8fad1cbef4cad3efe2f2ac9
emilykidd3 <e.kidd61@yahoo.com>
2021-07-18 21:55:04 -0700

    started alg 25

--- 25-reverse-in-place/Unsolved/reverse-in-place.js
+++ 25-reverse-in-place/Unsolved/reverse-in-place.js
@@ -4,4 +4,22 @@
 // element should be moved to arr.length - i
 // return original array
 
-var reverseInPlace = function(arr) {};
+var arr = [2, 4, 6, 8];
+// should return arr = [8, 6, 4, 2]
+
+var reverseInPlace = function(arr) {
+    let i = 0
+    var arr2 = [] 
+    for (i = 0; i < arr.length; i++) {
+        arr2[i] = arr[i];
+      }
+    while( i < arr.length) {
+        [arr[i]] = [arr2[arr.length - (i + 1)]]
+        [arr[arr.length - (i + 1)]] = [arr2[i]]
+        i++
+    }
+    console.log(arr);
+    return arr;
+};
+
+reverseInPlace(arr)
