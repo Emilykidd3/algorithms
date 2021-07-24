b747cd8cbeb88f67afa88aa611ebfc2431f4beb0
emilykidd3 <e.kidd61@yahoo.com>
2021-07-23 21:33:44 -0700

    pseudocoded alg

--- 26-look-say-sequence/Unsolved/look-and-say-sequence.js
+++ 26-look-say-sequence/Unsolved/look-and-say-sequence.js
@@ -1,5 +1,12 @@
 // Write code to create a function that accepts a number (`n`) and returns a new number that satisfies the look-and-say-sequence
 
+// create a var for current number as 1
+// loop through number?
+// if number is equal to next number
+// add 1 to current number var
+// if not equal add current number and number to new look and say variable
+//  return look and say number
+
 var lookAndSay = function(n) {
 
 };
