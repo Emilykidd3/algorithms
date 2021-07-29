589137bf5ced58f9c95aa2d379488e966c98fcea
emilykidd3 <e.kidd61@yahoo.com>
2021-07-28 22:14:33 -0700

    added next decimal number

--- 27-is-numeric/Unsolved/is-numeric.js
+++ 27-is-numeric/Unsolved/is-numeric.js
@@ -8,13 +8,13 @@
 // if decimal variable is not 0 or 1 return false
 // return true after loop if no false is returned
 
-var str = "13";
+var str = "-13.329";
 
 var isNumeric = function(str) {
     var decimalNumber = 0;
     for (var i = 0; i < str.length; i++) {
         console.log(str[i])
-        if (str[i] != 0 && str[i] != 1 && str[i] != 2 && str[i] != 3 && str[i] != 4 && str[i] != 5 && str[i] != 6 && str[i] != 7 && str[i] != 8 && str[i] != 9 && str[i] != "-" && str[i] != "+") {
+        if (str[i] != 0 && str[i] != 1 && str[i] != 2 && str[i] != 3 && str[i] != 4 && str[i] != 5 && str[i] != 6 && str[i] != 7 && str[i] != 8 && str[i] != 9 && str[i] != "-" && str[i] != "+" && str[i] != ".") {
             console.log("false");
             return false;
         }
@@ -22,9 +22,13 @@ var isNumeric = function(str) {
             console.log("false");
             return false;
         }
+        if (str[i] === ".") {
+            decimalNumber += 1;
+            console.log(decimalNumber);
+        }
     }
     console.log("true")
-    return true
+    return true;
 };
 
 isNumeric(str);
