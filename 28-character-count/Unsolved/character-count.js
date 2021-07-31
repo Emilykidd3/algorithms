9fff360a520d400f57793053525b845fa07f1f71
emilykidd3 <e.kidd61@yahoo.com>
2021-07-31 00:34:38 -0700

    pseudocoded alg 28

--- 28-character-count/Unsolved/character-count.js
+++ 28-character-count/Unsolved/character-count.js
@@ -1,3 +1,9 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
 
+// create object to be returned as charCount
+// loop through str and add each letter to object as key value pair
+// key is letter value is number 1
+// if letter/ value is already there, add one to value
+// return charCount object
+
 var characterCount = function(str) {};
