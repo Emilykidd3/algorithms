8394b7534984ec6cbf1f94631b60078d86b53d87
emilykidd3 <e.kidd61@yahoo.com>
2021-08-07 21:51:32 -0700

    pseudocoded alg and added second function

--- 29-product-of-largest-two/Unsolved/product-of-largest-two.js
+++ 29-product-of-largest-two/Unsolved/product-of-largest-two.js
@@ -1,3 +1,13 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
+// create variables for the two largest numbers
+// create a function to find the largest number
+
+// run function to find largest number assign to largest num var
+// remove largest number from arr and run function again assign to second largest num var
+// multiply largest numbers togther 
+// return result
+
 var productOfLargestTwo = function(arr) {};
+
+var findLargestNumber = function(num) {};
