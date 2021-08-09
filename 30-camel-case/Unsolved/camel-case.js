6d75d0990fe27c773c6e1e2b6ed919326a0c4887
emilykidd3 <e.kidd61@yahoo.com>
2021-08-08 21:43:51 -0700

    pseudocoded alg

--- 30-camel-case/Unsolved/camel-case.js
+++ 30-camel-case/Unsolved/camel-case.js
@@ -1,3 +1,10 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
+// split the string into an array using the space
+// add first word into new string
+// loop through the rest of the words 
+// capitalize the first letter of the word
+// add word to new string
+// return new string
+
 var camelCase = function(str) {};
