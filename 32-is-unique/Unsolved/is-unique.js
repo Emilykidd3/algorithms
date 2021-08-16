941c6a15360b8dbca99fc2096997a786119309d1
emilykidd3 <e.kidd61@yahoo.com>
2021-08-15 21:42:10 -0700

    pseudocoded alg 32

--- 32-is-unique/Unsolved/is-unique.js
+++ 32-is-unique/Unsolved/is-unique.js
@@ -1,4 +1,11 @@
 // Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
+// create an object numberCount
+// loop though arr and add each number as key value pair
+// key is number and value is 1
+// if number is already there add one to value
+// loop through object and if any key is > 1 return false 
+// return true
+
 var isUnique = function(arr) {};
