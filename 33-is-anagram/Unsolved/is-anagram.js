62c9af53eea754c266ac4bbb1ef15c1ae42e6134
emilykidd3 <e.kidd61@yahoo.com>
2021-08-16 22:29:51 -0700

    pseudocoded alg 32

--- 33-is-anagram/Unsolved/is-anagram.js
+++ 33-is-anagram/Unsolved/is-anagram.js
@@ -1,4 +1,8 @@
 // Write code to create a function that accepts two strings
 // Return true if the strings are anagrams of each other, otherwise return false
 
+// if string lengths are not the same return false
+// split reverse join one string and return false if they are not equal
+// return true
+
 var isAnagram = function(strA, strB) {};
