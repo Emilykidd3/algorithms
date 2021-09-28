accf45f0adab59b70c40b5355a3f98f73c7bb7d9
emilykidd3 <e.kidd61@yahoo.com>
2021-09-28 16:43:36 -0700

    pseudocoded alg 34

--- 34-most-songs-in-playlist/Unsolved/most-songs-in-playlist.js
+++ 34-most-songs-in-playlist/Unsolved/most-songs-in-playlist.js
@@ -2,19 +2,13 @@
 // Each number represents the duration of a song in minutes
 // Return the maximum number of songs from the array that can fit in a 60 minute playlist
 
-var noMoreThanPrevious = function(str) {
-  var letterCount = {};
+// pseudocode
+// sort arr smallest to largest
+// name a number of songs variable
+// name a time length variable
+// add 1st song time length to time length variable and add 1 to number of songs varibale
+// if time is less than 60 minutes plus next song time, add next song and add one to number of songs variable
+// else return time length variable.
 
-  var currentLetter = arr[0];
-  var prevLetter;
-  for (var i = 1; i < str.length; i++) {
-    var letter = str[i];
-    letterCount[letter] = (letterCount[letter] || 0) + 1;
-
-    if (prevLetter) {
-      if (letterCount[letter] > letterCount[letter]) {
-        return false;
-      }
-    }
-  }
+var noMoreThanPrevious = function (arr) {
 };
