c769b96464f83858cb75f08819028726a15f15d9
emilykidd3 <e.kidd61@yahoo.com>
2021-10-04 21:10:02 -0700

    pseudocoded alg

--- 35-is-perfect-square/Unsolved/is-perfect-square.js
+++ 35-is-perfect-square/Unsolved/is-perfect-square.js
@@ -1,4 +1,11 @@
 // Write code to create a function that accepts a number and returns true if the number is a perfect square, otherwise it returns false.
 // You may not use the built-in Math.sqrt method
 
-var isPerfectSquare = function(num) {};
+// pseudocode
+// make variable for squared num
+// if given num in less than squared num return false
+// if given num is greater than square num
+// loop through numbers starting at 1, if squared number is equal to given number, return true
+// at end return false
+
+var isPerfectSquare = function (num) { };
