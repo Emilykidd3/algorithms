00fb849433654d436602cb198e7af55abd46aae4
emilykidd3 <e.kidd61@yahoo.com>
2021-10-04 21:24:11 -0700

    solved alg 35

--- 35-is-perfect-square/Unsolved/is-perfect-square.js
+++ 35-is-perfect-square/Unsolved/is-perfect-square.js
@@ -3,9 +3,22 @@
 
 // pseudocode
 // make variable for squared num
+// loop through numbers starting at 1, if squared number is equal to given number, return true
 // if given num in less than squared num return false
 // if given num is greater than square num
-// loop through numbers starting at 1, if squared number is equal to given number, return true
 // at end return false
 
-var isPerfectSquare = function (num) { };
+var num = 37;
+
+var isPerfectSquare = function (num) {
+    for (var i = 1; (i * i) <= num; i++) {
+        if (i * i === num) {
+            console.log("true")
+            return true;
+        }
+    };
+    console.log("false")
+    return false;
+};
+
+isPerfectSquare(num);
