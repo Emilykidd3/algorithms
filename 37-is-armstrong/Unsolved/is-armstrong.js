08465915d3d229a55dca0842e5f30303f48aa735
emilykidd3 <e.kidd61@yahoo.com>
2021-10-06 22:40:58 -0700

    pseudocoded alg

--- 37-is-armstrong/Unsolved/is-armstrong.js
+++ 37-is-armstrong/Unsolved/is-armstrong.js
@@ -1,3 +1,9 @@
 // Write a function that takes a positive integer and returns true if the integer is an armstrong number, else return false. To find out if a number is an armstrong number, take each individual digit and raise it to the power of the length of the entire number and add the digits. If the sum equals the original number, the number is an armstrong number
 
-var isArmstrong = function(num) {};
+// pseudocode
+// armstrong number
+// take each digit and raise it to the power of the number of digits the number is
+// add to a variable 
+// if variable is equal to the original number return true
+
+var isArmstrong = function (num) { };
