9ad6a046635529a71d487318b22889f663b76ac7
emilykidd3 <e.kidd61@yahoo.com>
2021-10-10 16:50:04 -0400

    pseudocoded alg

--- 38-max-profit/Unsolved/max-profit.js
+++ 38-max-profit/Unsolved/max-profit.js
@@ -1,3 +1,11 @@
 // Write a function that takes an array of integers representing the price of a stock on different days. Return the maximum profit that can be made from buying and selling a single stock
 
-var maxProfit = function(prices) {};
+// pseudocode
+// make variable for max profit
+// loop through arr
+// loop through array again 
+// subtract 1st arr loop from second arr loop
+// if number is greater than max profit var, repace it
+// at end return max profit var
+
+var maxProfit = function (prices) { };
