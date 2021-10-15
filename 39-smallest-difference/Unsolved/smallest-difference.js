7580854013ac531122bcc68f2d9d973a639b724e
emilykidd3 <e.kidd61@yahoo.com>
2021-10-14 21:48:32 -0700

    pseudocoded alg

--- 39-smallest-difference/Unsolved/smallest-difference.js
+++ 39-smallest-difference/Unsolved/smallest-difference.js
@@ -1,3 +1,12 @@
 // Write a function that takes in two sorted arrays and returns a pair of numbers (one from each array) with the smallest difference
 
-var smallestDifference = function(arr1, arr2) {};
+// pseudocode
+// set variable to return arr
+// set var to smallest difference set as absolute value of arr1 of 1 minus arr2 of 2
+// loop through first arr
+// loop through second arr
+// sub arr1 from arr2 and get absolute value
+// if it is smaller than the var, replace var and use i and j as the new arr to return
+// return arr
+
+var smallestDifference = function (arr1, arr2) { };
