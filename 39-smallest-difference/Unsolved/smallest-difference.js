108edb297847120219b66cc3b7892fcfa89b3e2b
emilykidd3 <e.kidd61@yahoo.com>
2021-10-15 15:29:28 -0700

    added and tested for loops

--- 39-smallest-difference/Unsolved/smallest-difference.js
+++ 39-smallest-difference/Unsolved/smallest-difference.js
@@ -15,7 +15,12 @@ var arr2 = [8, 9, 10, 11, 12, 13];
 var smallestDifference = function (arr1, arr2) {
     var arrToReturn = [];
     var smallestDiff = Math.abs(arr1[0] - arr2[0]);
-    console.log(smallestDiff)
+    for (var i = 0; i < arr1.length; i++) {
+        for (var j = 0; j < arr2.length; j++) {
+            console.log(arr1[i])
+            console.log(arr2[j])
+        }
+    }
 };
 
 smallestDifference(arr1, arr2)
