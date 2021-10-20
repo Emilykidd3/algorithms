b3a0ebb901a65bf5739d7e232a310f2f8045eb6b
emilykidd3 <e.kidd61@yahoo.com>
2021-10-19 22:27:33 -0700

    pseudocoded alg 40

--- 40-merge-meeting-times/Unsolved/merge-meeting-times.js
+++ 40-merge-meeting-times/Unsolved/merge-meeting-times.js
@@ -1,3 +1,10 @@
 // Write a function that takes in a 2D array of meeting times, where each sub array contains 2 integers representing a meeting start and end time. Return a new 2D array such that overlapping meeting blocks are condensed into combined meeting blocks
 
-var mergeMeetingTimes = function(arr) {};
+// pseudocode
+// sort sub arrs by start time
+// loop through arrs
+// if end time is after the next start time, replace end time with next end time and remove next arr
+// if end time is before next start time go to next in loop
+// after loop return arr
+
+var mergeMeetingTimes = function (arr) { };
