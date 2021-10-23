1ba79ee4c0c3db6047cb4445cbb074e12234b4b4
emilykidd3 <e.kidd61@yahoo.com>
2021-10-22 21:38:20 -0700

    stopped arr loop at second to last sub arr, replaced second sub arr char when previous meeting time ends after current meeting starts

--- 40-merge-meeting-times/Unsolved/merge-meeting-times.js
+++ 40-merge-meeting-times/Unsolved/merge-meeting-times.js
@@ -13,11 +13,13 @@ var mergeMeetingTimes = function (arr) {
     var newArr = arr.sort((a, b) => {
         return a[0] - b[0];
     })
-    for (var i = 0; i < newArr.length; i++) {
-        if (newArr[i][1] > newArr[i][0]) {
-            console.log("end time is after next meeting start time")
+    for (var i = 0; i < newArr.length - 1; i++) {
+        if (newArr[i][1] > newArr[i + 1][0]) {
+            console.log("here")
+            newArr[i][1] = newArr[i + 1][1];
         }
     }
+    console.log(newArr);
 };
 
 mergeMeetingTimes(arr)
