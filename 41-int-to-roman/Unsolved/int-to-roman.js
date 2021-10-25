68bf86166a46503488442e352bb95afae168dee6
emilykidd3 <e.kidd61@yahoo.com>
2021-10-24 21:49:40 -0700

    started pseudocode and added chart for roman numberals

--- 41-int-to-roman/Unsolved/int-to-roman.js
+++ 41-int-to-roman/Unsolved/int-to-roman.js
@@ -1,3 +1,10 @@
 // Write a function that takes in an integer and returns it as a Roman numeral string
 
-var intToRoman = function(num) {};
+// roman numerals
+
+
+// pseudocode
+// loop through number from last digit to first digit
+// 
+
+var intToRoman = function (num) { };
