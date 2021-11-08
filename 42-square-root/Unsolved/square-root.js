7c5525412c8ed2ab8e988cd56f55da084876da09
emilykidd3 <e.kidd61@yahoo.com>
2021-11-07 21:21:14 -0800

    pseudocoded alge 42

--- 42-square-root/Unsolved/square-root.js
+++ 42-square-root/Unsolved/square-root.js
@@ -1,4 +1,9 @@
 // Write code to create a function that accepts a non-negative integer and returns the square root of the integer. If the square root is a decimal number, round down to the nearest whole integer
 // You may not use the built-in `Math.sqrt` method
 
-var sqrt = function(x) {};
+// pseudocode
+// loop through numbers starting at 1
+// if loop squared is less than x keep looping
+// if loop squared is greater than x return i-1
+
+var sqrt = function (x) { };
