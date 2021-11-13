29fba4b0e9f6eaff8e3e15cef1918746c1b3cf96
emilykidd3 <e.kidd61@yahoo.com>
2021-11-12 21:34:56 -0800

    pseudocoded alg 43

--- 43-needle-in-the-haystack/Unsolved/needle-in-the-haystack.js
+++ 43-needle-in-the-haystack/Unsolved/needle-in-the-haystack.js
@@ -2,4 +2,15 @@
 // If the second string is found inside of the first string, return its starting index in the first string
 // Else return `-1`
 
+//pseudocode
+// if arr2 is empty return 0
+// loop through arr 1
+// if arr2[0] is in arr1
+// loop through arr2 and arri[i+j] to see if equal
+// if equal, return i
+
+// other notes
+// check if first letter is in arr1 1st?
+
+
 var strStr = function(str1, str2) {};
