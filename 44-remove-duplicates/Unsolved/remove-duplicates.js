ad77ae1eb55ec06ecb20ec73b55b8de34814b926
emilykidd3 <e.kidd61@yahoo.com>
2021-11-17 21:39:58 -0800

    pseudocoded alg 44

--- 44-remove-duplicates/Unsolved/remove-duplicates.js
+++ 44-remove-duplicates/Unsolved/remove-duplicates.js
@@ -2,4 +2,12 @@
 // Do not create a new array, instead modify the original array
 // There is no need to return anything from this function
 
+// loop through arr
+// if number and next number are the same
+// remove first number
+// console.log arr
+
+// how to remove something from an arr
+// arr.splice(i+1, 0)
+
 var removeDuplicates = function(nums) {};
