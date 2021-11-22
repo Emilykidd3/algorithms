f9079bd0abe1d295d4f5a2ab93d2613a5369ba02
emilykidd3 <e.kidd61@yahoo.com>
2021-11-21 21:29:26 -0800

    pseudocoded alg 45

--- 45-valid-brackets/Unsolved/valid-brackets.js
+++ 45-valid-brackets/Unsolved/valid-brackets.js
@@ -1,3 +1,14 @@
 // Write a function to take a string and return true if it contains valid sets of matching brackets, else return false
 
+// pseudocode
+// split string into arr
+// if arr length is odd, return false
+// run remove closed pair function
+// if arr length is 0 return true
+// loop through arr
+// if arr[i] and arr[i + 1] are equal to () or [] or {}
+// remove arr[i] and arr[i+1] from arr
+// else if return false
+
+
 var validBrackets = function(str) {};
