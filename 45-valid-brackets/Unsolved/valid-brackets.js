3815a11aca14fe95b55de2df8d2f2b3f1baa9738
emilykidd3 <e.kidd61@yahoo.com>
2021-11-21 21:37:47 -0800

    added second function to remove pairs of brackets, added split to first funciton

--- 45-valid-brackets/Unsolved/valid-brackets.js
+++ 45-valid-brackets/Unsolved/valid-brackets.js
@@ -10,5 +10,20 @@
 // remove arr[i] and arr[i+1] from arr
 // else if return false
 
+var str = "{ } [ ( [] ) ]";
 
-var validBrackets = function(str) {};
+var validBrackets = function(str) {
+    var arr = str.split("");
+    removeClosedPair(arr);
+};
+
+var removeClosedPair= function(arr){
+    for (var i=0; i < arr.length; i++){
+        if (arr[i] === "(" && arr[i+1] === ")"){
+            arr.splice(i, 2);
+        }
+    }
+    console.log(arr);
+}
+
+validBrackets(str);
