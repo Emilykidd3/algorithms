55679ff4e714420f198467203648b284bfe9e085
emilykidd3 <e.kidd61@yahoo.com>
2021-11-27 21:56:00 -0800

    pseudocoded alg 46

--- 46-min-increment-for-unique/Unsolved/min-increment-for-unique.js
+++ 46-min-increment-for-unique/Unsolved/min-increment-for-unique.js
@@ -1,3 +1,12 @@
 // Write a function to take an array of integers and return the minimum number of increments required across the array to ensure that every number is unique
 
+// pseudocode
+// sort arr from smallest to largest
+// make variable for how much is incremented
+// loop through arr
+// if arr[i] is equal to arr[i+1]
+// sort arr
+// add 1 to arr[i+1] and add 1 to variable
+// return variable 
+
 var minIncrement = function(nums) {};
