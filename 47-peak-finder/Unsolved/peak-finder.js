80bebac3a0592ccd0cc6ea74dca433ccac627188
emilykidd3 <e.kidd61@yahoo.com>
2021-11-29 21:11:45 -0800

    pseudocoded alg 47

--- 47-peak-finder/Unsolved/peak-finder.js
+++ 47-peak-finder/Unsolved/peak-finder.js
@@ -1,3 +1,7 @@
 // Write a function that takes an array of integers containing exactly one peak. A peak is defined as a location in the array where the value is greater than every number to the left and every number to the right. Return the value found at the array's peak
 
+// pseudocode
+// sort arr from smallest to largest
+// return last number 
+
 var peakFinder = function(nums) {};
