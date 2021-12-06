ba27f789a42d8d70f2419b44488750addfa4dd66
emilykidd3 <e.kidd61@yahoo.com>
2021-12-05 21:39:42 -0800

    pseudocoded alg 48 and added roman numeral key

--- 48-roman-to-int/Unsolved/roman-to-int.js
+++ 48-roman-to-int/Unsolved/roman-to-int.js
@@ -1,3 +1,37 @@
 // Write a function that takes in a Roman Numeral string and returns its integer form
 
+// roman numeral key
+// 1 = I
+// 5 = V
+// 10 = X
+// 50 = L
+// 100 = C
+// 500 = D
+// 1000 = M
+// 3000 = MMM
+
+// pseudocode
+// make variable for number
+// if there is an M add 1000 to var
+// if there is a C before the M, subtract 100
+// if there is a C after the M, add 100
+// if there is a D add 500
+// if there is a C before the D, subtract 100
+// if there is a C after the D, add 100
+// if there is a C add 100
+// if there is a X before the C, subtract 10
+// if there is a X after the C, add 10
+// if there is a L add 50
+// fi there is a X before the L, subtract 10
+// if there is a X after hte L, add 10
+// if there is an X add 10
+// if there is an I before the X, subtract 1
+// if there is an I after the X, add 1
+// if there is a V add 5
+// if there is an I before the V, subtract 1
+// if there is an I after the V, add 1
+// if there is an I add 1
+
+// var str = "MMXXIV";
+
 var romanToInt = function(str) {};
