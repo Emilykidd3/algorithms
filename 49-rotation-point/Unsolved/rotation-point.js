052b2a5c5f8da6aeba9f7df29ab20f4800b49240
emilykidd3 <e.kidd61@yahoo.com>
2021-12-12 17:20:00 -0800

    pseudocoded alg

--- 49-rotation-point/Unsolved/rotation-point.js
+++ 49-rotation-point/Unsolved/rotation-point.js
@@ -1,3 +1,9 @@
 // Write a function that takes in an array of sorted strings and returns the index of its rotation point if it has one, else return -1
 
+// pseudocode
+// make variable for rotation index
+// loop through first letter of each string in the array
+// if numerical value of string is greater than string+1 add index to variable
+// return variable
+
 var rotationPoint = function(words) {};
