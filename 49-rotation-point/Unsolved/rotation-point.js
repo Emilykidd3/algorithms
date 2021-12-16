be3f8c2eb330612d2d436182040f2f0db3c5e2f6
emilykidd3 <e.kidd61@yahoo.com>
2021-12-15 21:27:45 -0800

    added if statment for words that start with the same letter and return for ordered arrays

--- 49-rotation-point/Unsolved/rotation-point.js
+++ 49-rotation-point/Unsolved/rotation-point.js
@@ -6,20 +6,28 @@
 // if numerical value of string is greater than string+1 add index to variable
 // return variable
 
-var words = ["cool", "cat"];
+var words = ["great", "ostrich", "panther", "ruby", "chosen", "feathers"];
 
 var rotationPoint = function(words) {
     var rotationNum = 0;
     for (var i = 0; i < words.length-1; i++) {
-        if (words[i][0] < words[i+1][0]){
+        if (words[i][0] > words[i+1][0]){
             console.log("before")
         } else if (words[i][0]===words[i+1][0]){
             for (var j=0; j < words[i].length && i<words[i+1].length; j++){
-                console.log("here")
+                if (words[i][j] > words[i+1][j]){
+                    console.log(i+1);
+                    return i+1;
+                } else if (words[i][j] < words[i+1][j]){
+                    console.log(-1);
+                    return -1;
+                }
             }
+        } else {
+            console.log("here")
         }
-        console.log(words[i][0]);
     }
+    console.log(-1);
 };
 
 rotationPoint(words);
