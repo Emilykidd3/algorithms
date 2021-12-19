d615caf6f05458526cb3cade14afb9b33d276626
emilykidd3 <e.kidd61@yahoo.com>
2021-12-18 22:03:55 -0800

    started pseudocode for alg 50

--- 50-simplify-path/Unsolved/simplify-path.js
+++ 50-simplify-path/Unsolved/simplify-path.js
@@ -1,13 +1,16 @@
 // Write a function that takes in an absolute file path and simplifies it
 
 // key
-// ../ takes you back a directory
-// ./ keeps in same directory
+// .. takes you back a directory
+// . keeps in same directory
 // / at the beginning takes you to the root directory
 // / at the end does nothing?
 // 
 
 // pseudocode
 // if empty return "/"
+// if there is /.. take out letter before it
+// if there is /. take it out
+// if there is a / at the end, remove it
 
 var simplifyPath = function(path) {};
