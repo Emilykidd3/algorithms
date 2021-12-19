dc425c146838d4d933d7af82e169eeba07a6a862
emilykidd3 <e.kidd61@yahoo.com>
2021-12-18 22:01:50 -0800

    added key for alg 50 file paths

--- 50-simplify-path/Unsolved/simplify-path.js
+++ 50-simplify-path/Unsolved/simplify-path.js
@@ -1,3 +1,13 @@
 // Write a function that takes in an absolute file path and simplifies it
 
+// key
+// ../ takes you back a directory
+// ./ keeps in same directory
+// / at the beginning takes you to the root directory
+// / at the end does nothing?
+// 
+
+// pseudocode
+// if empty return "/"
+
 var simplifyPath = function(path) {};
