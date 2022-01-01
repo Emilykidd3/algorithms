e559da62c6ace8b7010662d0dac99467823c138b
emilykidd3 <e.kidd61@yahoo.com>
2021-12-31 21:26:52 -0800

    removed unnecessary if statement, fixed looping bug, commented in removing the ending slash

--- 50-simplify-path/Unsolved/simplify-path.js
+++ 50-simplify-path/Unsolved/simplify-path.js
@@ -17,7 +17,7 @@
 // two periods make you go back a directory
 // one period keeps you in the same directory
 
-var path = "/a/../../b/../c//.//";
+var path = "/a/b/c/d/..";
 // should return "/C"
 
 var simplifyPath = function (path) {
@@ -66,29 +66,28 @@ var simplifyPath = function (path) {
       path[i + 1] === "." &&
       path[i + 2] === "."
     ) {
-        var amountToSubtract = 0;
+      var amountToSubtract = 0;
       for (var j = 0; j < path.length - i + 1; j++) {
         if (path[i - j] != "/" && path[i - j] != "." && path[i - j]) {
           amountToSubtract += 1;
         }
-    }
-      if (path[i - 1] && path[i - 1] != "." && path[i - 1] != "/") {
-        var path1 = path.substr(0, i - amountToSubtract);
-        var path2 = path.substr(i + 3, path.length);
-        path = path1 + path2;
-        i -= 3;
-        amountToSubtract = 0;
       }
-    } else if (!path[i - 1] && path[i] === "/" &&
-    path[i + 1] === "." &&
-    path[i + 2] === "." && path[i+3]) {
+      var path1 = path.substr(0, i - amountToSubtract);
+      var path2 = path.substr(i + 3, path.length);
+      path = path1 + path2;
+      i = 0;
+      amountToSubtract = 0;
+    } else if (
+      path[i] === "/" &&
+      path[i + 1] === "." &&
+      path[i + 2] === "." &&
+      path[i + 3]
+    ) {
       var path1 = path.substr(0, i + 1);
       var path2 = path.substr(i + 3, path.length);
       path = path1 + path2;
-      i -= 1;
-    } else if (!path[i - 1] && path[i] === "/" &&
-    path[i + 1] === "." &&
-    path[i + 2] === ".") {
+      i = 0;
+    } else if (path[i] === "/" && path[i + 1] === "." && path[i + 2] === ".") {
       var path1 = path.substr(0, i + 1);
       var path2 = path.substr(i + 3, path.length);
       path = path1 + path2;
@@ -118,19 +117,19 @@ var simplifyPath = function (path) {
   }
 
   // if there are two / in a row, delete the first one
-  for (var i = 0; i<path.length; i++) {
-      if (path[0] === "/" && path [1] === "/") {
-          path = path.substr(1, path.length);
-          i -=1;
-      }
+  for (var i = 0; i < path.length; i++) {
+    if (path[0] === "/" && path[1] === "/") {
+      path = path.substr(1, path.length);
+      i -= 1;
+    }
   }
 
-
-//   for (var i=0; i < path.length; i++){
-//       if (path[path.length-1] === "/" && path.length > 1){
-//           path = path.substr(0, path.length-1)
-//       }
-//   }
+  // if the last character is a /, remove it
+  for (var i = 0; i < path.length; i++) {
+    if (path[path.length - 1] === "/" && path.length > 1) {
+      path = path.substr(0, path.length - 1);
+    }
+  }
   console.log(path);
 };
 
