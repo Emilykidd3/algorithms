0d7c0db908a70648f226568b29d0cce2aff56bd5
emilykidd3 <e.kidd61@yahoo.com>
2022-01-02 21:41:59 -0800

    pseudocoded alg 51

--- 51-plus-one/Unsolved/plus-one.js
+++ 51-plus-one/Unsolved/plus-one.js
@@ -2,5 +2,13 @@
 // Return the new array
 // Solve without joining the digits array and modify the original array in place
 
+// notes
+// want function to be a loop! so if arr is [9, 9, 9, 9, ..., 9] it will keep going until arr is over
+
+// pseudocode
+// loop through the array from end to start
+// if arr[arr.length] is 8 or less, add one and RETURN
+// if arr[arr.length] is 9, set it equal to 0, (or subtract 9?) and move on to previous number
+
 var plusOne = function(digits) {
 };
