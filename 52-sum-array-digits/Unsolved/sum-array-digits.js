3dbac802fd9fd4b4c7b97a3c2ef554f19b53ea1e
emilykidd3 <e.kidd61@yahoo.com>
2022-01-08 21:45:04 -0800

    pseudocoded alg 52

--- 52-sum-array-digits/Unsolved/sum-array-digits.js
+++ 52-sum-array-digits/Unsolved/sum-array-digits.js
@@ -4,4 +4,21 @@
 // Create and return a new array that is the sum of the numbers of the two provided arrays
 // Solve this problem without type coercion, ie don't join the array into a string
 
-var sumArrayDigits = function(arr1, arr2) {};
+// pseudocode
+// make variable for new arr
+// loop through arrs starting from end
+// if arr1 and arr2 exist
+// add the digit of arr1 to the last or arr2 and numToAdd
+// if the total is greater than 9, unshift the second number to the new Arr and add one to arr1[i-1] 
+// if arr1 exists and arr2 doesnt or opposite
+// unshift number to newArr
+
+var arr1 = [1, 0];
+
+var arr2 = [1];
+
+var sumArrayDigits = function(arr1, arr2) {
+    var newArr = [];
+};
+
+sumArrayDigits(arr1, arr2)
