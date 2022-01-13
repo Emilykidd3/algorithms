42080b5b3e6946332dd52374c3f24cfa64f92cdd
emilykidd3 <e.kidd61@yahoo.com>
2022-01-12 18:23:14 -0800

    pseudocoded alg 53

--- 53-one-edit-away/Unsolved/one-edit-away.js
+++ 53-one-edit-away/Unsolved/one-edit-away.js
@@ -1,3 +1,12 @@
 // Write a function that takes in two strings and returns true if they are one edit away, else return false
 
+// pseudocode
+// if str1 and str2 are the same, return false
+// make variable for amount of edits set to 0
+// loop through strings
+// if str1[i] is not equal to str2[2] add one to amount of edits variable
+// if variable is > 1 return false
+// at end if variable does not equal to 1 return false
+// else return true
+
 var oneEditAway = function(str1, str2) {};
