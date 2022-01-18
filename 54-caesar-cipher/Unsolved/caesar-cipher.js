4b23dcf52a9df9ec094952690b58db5fb78578da
emilykidd3 <e.kidd61@yahoo.com>
2022-01-17 20:55:20 -0800

    pseudocoded alg 54

--- 54-caesar-cipher/Unsolved/caesar-cipher.js
+++ 54-caesar-cipher/Unsolved/caesar-cipher.js
@@ -1,3 +1,20 @@
 // Write a function that takes in a string and and an offset and returns a new string with each character moved by the offset
 
-var caesarCipher = function(str, offset) {};
+// pseudocode
+// split string into arr
+// while offset is greater than or equal to 26, subtract 26
+// loop through arr
+// a-z is 97-122
+// A-Z is 65-90
+// if statements for cap vs lower?
+// replace letter with char code using .charCodeAt() 
+// add offset to each number
+// if number is greater than 122, subtract 26
+// replace number with .fromCharCode
+// join arr
+// return
+
+var caesarCipher = function(str, offset) {
+    
+};
+caesarCipher(str, offset);
