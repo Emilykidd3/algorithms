59baba4320a17502b9caf726fd5432233196ccc2
emilykidd3 <e.kidd61@yahoo.com>
2022-03-02 17:41:04 -0800

    added some common array minipulation methods

---
+++ arrayminip.js
@@ -0,0 +1,18 @@
+let arr = [1, 2, 3];
+
+arr.push(4);
+// returns [1, 2, 3, 4]
+
+arr.pop();
+// returns [1, 2, 3]
+
+arr.shift();
+// returns [2, 3]
+
+arr.unshift(1);
+// returns [1, 2, 3]
+
+console.log(['a', 'b'].concat('c'))
+// returns ['a', 'b', 'c']
+
+console.log(arr);
