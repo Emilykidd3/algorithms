93d0c66dc0221fd4aa4fef04e9280578058df267
emilykidd3 <e.kidd61@yahoo.com>
2022-03-23 22:59:24 -0700

    pseudocoded and solved alg 55

---
+++ 55-reerse-string/index.js
@@ -0,0 +1,17 @@
+// Given a string of characters as input, write a function that returns it with the characters reversed.
+
+// given var arr = [1, 2, 3, 4]
+// expected return is arr = [4, 3, 2, 1]
+
+var arr = [1, 2, 3, 4]
+
+function reverseArr(arr) {
+    var newArr = []
+    for (var i = arr.length-1; i > -1; i--){
+        newArr.push(arr[i]);
+    }
+    arr = newArr;
+    console.log(arr);
+}
+
+reverseArr(arr)
