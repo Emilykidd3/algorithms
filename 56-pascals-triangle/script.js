2d6db53bc73a79b1395740a00d9ad809089746d4
emilykidd3 <e.kidd61@yahoo.com>
2022-07-05 21:05:41 -0700

    added new alg, started pseudocode, and actual code for first row

---
+++ 56-pascals-triangle/script.js
@@ -0,0 +1,20 @@
+// n=0; 1
+// n=1; 1 1
+// make alg to find n of any number for pascals triangle
+
+// given n = 4
+
+// pseudocode
+// recursive function?
+// loop through n times?
+
+var n = 0
+
+function findRow(n) {
+    if (n===0) {
+        console.log(1)
+        return 1;
+    }
+}
+
+findRow(n);
