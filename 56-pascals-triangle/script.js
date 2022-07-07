edace09bfe1b19725fa557bf5bfdf4d7d597a1a1
emilykidd3 <e.kidd61@yahoo.com>
2022-07-06 21:57:07 -0700

    added if case for n = 1, added second function for adding a row

--- 56-pascals-triangle/script.js
+++ 56-pascals-triangle/script.js
@@ -7,13 +7,33 @@
 // pseudocode
 // recursive function?
 // loop through n times?
+// if there is no number before or after add 0?
+// if there is a number add that
 
-var n = 0
+var n = 1
 
 function findRow(n) {
+    var oldRow = [1]
+    var newRow= [1, 1]
     if (n===0) {
-        console.log(1)
-        return 1;
+        console.log(oldRow)
+        return oldRow;
+    } 
+    if (n===1){
+        console.log(newRow)
+        return newRow;
+    }
+    for (var i = 0; i < n; i ++){
+        newRow = addRow(oldRow);
+    }
+}
+
+function addRow(oldRow) {
+    var newRow = []
+    for (var j = 0; j < oldRow.length; j++){
+        // take first num of oldRow arr
+        // push to 
+        console.log(newRow)
     }
 }
 
