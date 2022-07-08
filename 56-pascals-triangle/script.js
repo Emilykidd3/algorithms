a201226fe1397e073b725526733aedd027f7de17
emilykidd3 <e.kidd61@yahoo.com>
2022-07-07 23:01:11 -0700

    updated if statements for rows one and two, added pushes for newRow arr for n = 3

--- 56-pascals-triangle/script.js
+++ 56-pascals-triangle/script.js
@@ -10,31 +10,37 @@
 // if there is no number before or after add 0?
 // if there is a number add that
 
-var n = 1
+var n = 2
 
 function findRow(n) {
-    var oldRow = [1]
-    var newRow= [1, 1]
+    var oldRow = [1, 1]
+    var newRow= [1]
     if (n===0) {
-        console.log(oldRow)
-        return oldRow;
-    } 
-    if (n===1){
         console.log(newRow)
         return newRow;
+    } 
+    if (n===1){
+        console.log(oldRow)
+        return oldRow;
     }
-    for (var i = 0; i < n; i ++){
+    for (var i = 1; i < n; i ++){
         newRow = addRow(oldRow);
     }
 }
 
 function addRow(oldRow) {
     var newRow = []
-    for (var j = 0; j < oldRow.length; j++){
-        // take first num of oldRow arr
-        // push to 
-        console.log(newRow)
+    newRow.push(1);
+    for (var j = 0; j < oldRow.length - 1; j++){
+        // use old row
+        // take first number or 1 and push to newRow 
+        // add first number and second number and push to newRow
+        // keep going until length is over
+        // add one to end
+        newRow.push(oldRow[j] + oldRow[j+1])
     }
+    newRow.push(1);
+    console.log(newRow);
 }
 
 findRow(n);
